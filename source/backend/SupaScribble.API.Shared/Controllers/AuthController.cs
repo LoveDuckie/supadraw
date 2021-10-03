7b719d22c505eb7185122cfc330eebe850143e67
Luc <692378+LoveDuckie@users.noreply.github.com>
2021-10-03 19:21:57 +0100

    First-time submit. Project structuring.
    Nothing particularly interesting. Doing boilerplate work for project structuring.

---
+++ source/backend/SupaScribble.API.Shared/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SupaScribble.API.Controllers
+{
+    public sealed class AuthController : ControllerBase
+    {
+        public AuthController() : base()
+        {
+        }
+    }
+}
---
+++ source/frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AuthComponent } from './auth/auth.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    AboutComponent,
+    AuthComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
